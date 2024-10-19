de8934f7c55f81e75eeda70718e496e6186ae335
whosNikoloz <nika.kobaidze021@gmail.com>
2024-10-19 14:21:09 +0400

    init

---
+++ test/WeatherControllerTests.cs
@@ -0,0 +1,37 @@
+﻿using Moq;
+using UnitTestingExample;
+using UnitTestingExample.Controllers;
+using Microsoft.Extensions.Logging;
+
+
+namespace test
+{
+    public class WeatherControllerTests
+    {
+        private readonly WeatherForecastController _controller;
+
+        public WeatherControllerTests()
+        {
+            var loggerMock = new Mock<ILogger<WeatherForecastController>>();
+            _controller = new WeatherForecastController(loggerMock.Object);
+        }
+
+        [Fact]
+        public void Get_ShouldReturnWeatherForecasts()
+        {
+            var result = _controller.Get();
+
+            var forecasts = Assert.IsAssignableFrom<IEnumerable<WeatherForecast>>(result);
+            Assert.Equal(5, forecasts.Count()); 
+        }
+
+        [Fact]
+        public void Get_ShouldReturnWeatherForecast()
+        {
+            var result = _controller.Get(1);
+
+            var forecast = Assert.IsType<WeatherForecast>(result);
+            Assert.True(forecast == null);
+        }
+    }
+}
